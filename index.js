79c7d78bfc187bb6945fd418cfabe260afd1ff0b
pjbiradar <poojabiradar2412@gmail.com>
2021-11-20 23:10:02 +0530

    anytext

--- index.js
+++ index.js
@@ -1,122 +1,126 @@
+let t=""
+let t1=""
+let t2=""
+let t3=""
+
 const timer=()=>{
   let date=new Date();
   let hh=date.getHours();
   let min=date.getMinutes();
   let sec=date.getSeconds();
-  let x=hours>=12? 'pm' : 'am';
+  let x=hh>=12? 'pm' : 'am';
 
 
-  if(hours>=12){
-    hours -= 12;
+  if(hh>=12){
+    hh -= 12;
 
   }
-  else if(hours==0){
-    hours=12;
+  else if(hh==0){
+    hh=12;
     
   }
-
+  console.log(hh);
+  console.log(min);
+  console.log(sec);
   document.getElementById("hours").innerHTML=hh;
+  
   document.getElementById("min").innerHTML=min;
   document.getElementById("sec").innerHTML=sec+1;
   document.getElementById("amorpm").innerHTML=x;
-  // setTimeout(timer,1000)
-}
-setTimeout(timer,1000)
 
+ const store1=document.getElementById("amorpm")
+ const element= document.getElementById("hours")
 
-const hii=()=>
-{
-  const e=document.getElementById("a1");
-  e.setAttribute("class","a2");
-  console.log(e);
-
-  const c1=document.getElementById("a6");
-  c1.setAttribute("class","a5");
-
-  const c2=document.getElementById("a3");
-  c2.setAttribute("class","a4");
-
-  const c3=document.getElementById("a7");
-  c3.setAttribute("class","a8");
-
-  const change=()=>
+  switch(true)
   {
-    
-    const d=document.getElementById("timings").value;
-    console.log(d);
-    if(d=="")
-    {
+  case(t.includes(element)&&t.includes(store1)):
+  const g=document.getElementById("picnic");
+  g.src="wakeup.png"
+  const say=document.getElementById("five1")
+  say.innerText="lets have some breakfast"
+  break;
+  case(t1.includes(element)&&t1.includes(store1)):
+  const g1=document.getElementById("picnic");
+  g1.src="lunch.png"
+  const say1=document.getElementById("five1")
+  say1.innerText="lets have some lunch"
+  break;
+  case(t2.includes(element)&&t2.includes(store1)):
+  const g2=document.getElementById("picnic");
+  g2.src="night.png"
+  const say2=document.getElementById("five1")
+  say2.innerText="lets have supper"
+  break;
+  default:
+  const g3=document.getElementById("picnic");
+  g3.src="default image"
+  const say3=document.getElementById("five1")
+  say3.innerText="Hii buddy,Hope ur doing well :)"
+  break;
 
-    }
-    else{
-      document.getElementById("a6").innerText="wake up time:"+d;
-    }
-    
-    
-
-    const d1=document.getElementById("timings2").value;
-    console.log(d1);
-    if(d1=="")
-    {
-
-    }
-    else{
-      document.getElementById("a3").innerText="lunch time:"+d1;
-    }
-  
-    const d2=document.getElementById("timings3").value;
-    console.log(d2);
-    if(d2=="")
-    {
-
-    }
-    else
-    {
-      document.getElementById("a7").innerText="dinner time:"+d2;
-    }
   }
-  change()
+  
+  
 }
+setInterval(timer,1000);
 
 
+let child=hii();
 
+function hii ()
+{
+  let key=document.createElement("div");
+  key.setAttribute("id","key1");
+  key.style.height="110px";
+  key.style.width="250px";
+  key.style.backgroundColor="blue";
+  key.style.borderRadius="7px";
+  key.style.position="absolute";
+  key.style.bottom="20px";
+  key.style.left="160px";
 
-// const key=()=>{
-//   switch(true)
-//   {
-//   case(t.includes(element)&&t.includes(store1)):
-//   const g=document.getElementById("picnic");
-//   g.src="wakeup.png"
-//   const say=document.getElementById("five1")
-//   say.innerText="lets have some breakfast"
-//   break;
-//   case(t1.includes(element)&&t1.includes(store1)):
-//   const g1=document.getElementById("picnic");
-//   g1.src="lunch.png"
-//   const say1=document.getElementById("five1")
-//   say1.innerText="lets have some lunch"
-//   break;
-//   case(t2.includes(element)&&t2.includes(store1)):
-//   const g2=document.getElementById("picnic");
-//   g2.src="night.png"
-//   const say2=document.getElementById("five1")
-//   say2.innerText="lets have supper"
-//   break;
-//   case(t3.includes(element)&&t3.includes(store1)):
-//   const say3=document.getElementById("picnic");
-//   g3.src="default image"
-//   const say3=document.getElementById("five1")
-//   say3.innerText="Hello Girl,Hope ur doing Fine :)"
-//   break;
+  let morning=document.createElement("p");
+  morning.setAttribute("id", "morning");
+  morning.style.fontSize = "18px";
+  morning.style.color = "white";
+  morning.style.marginTop = "10px";
+  morning.style.marginLeft = "15px";
 
-//   }
+  let after=document.createElement("p");
+  after.setAttribute("id", "after");
+  after.style.fontSize = "18px";
+  after.style.color = "white";
+  after.style.marginTop = "10px";
+  after.style.marginLeft = "15px";
 
-// }
+  let night=document.createElement("p");
+  night.setAttribute("id", "night");
+  night.style.fontSize = "18px";
+  night.style.color = "white";
+  night.style.marginTop = "10px";
+  night.style.marginLeft = "15px";
+  return function change()
+  {
+    let z=document.getElementById("whole").appendChild(key)
+    console.log(z);
+    let j = document.getElementById("timings").value
+    morning.innerText = "Wake up : " + j
+    document.getElementById("key").appendChild(morning)
 
-// key()
+    let j1 = document.getElementById("timings2").value
+    after.innerText = "Lunch : " + j1
+    document.getElementById("key").appendChild(after)
 
+    let j3 = document.getElementById("timings3").value
+    night.innerText = "Nap : " + j3
+    document.getElementById("key").appendChild(night)
 
-
+    t = (document.getElementById("timings").value).slice(0, 4)
+    t1 = (document.getElementById("timings2").value).slice(0, 4)
+    t2 = (document.getElementById("timings3").value).slice(0, 4)
+  }
+}
+  
 
 
 
