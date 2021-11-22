9489d2e0a5ad80479db6962281b433249bd060f9
pjbiradar <poojabiradar2412@gmail.com>
2021-11-22 23:04:28 +0530

    anytext

--- index.js
+++ index.js
@@ -14,73 +14,75 @@ const timer=()=>{
     hh=12;
     
   }
- 
- 
   document.getElementById("hours").innerHTML=hh;
   // console.log(x);
   document.getElementById("min").innerHTML=min;
   document.getElementById("sec").innerHTML=sec+1;
   document.getElementById("amorpm").innerHTML=x;
 
+
+
   var d=document.getElementById("timings").value;
-  var value1=d.slice(0,1);
-  // console.log(value1);
+  var value1=d.slice(0,2);
+  console.log(value1);
+  console.log(hh);
   var local=d.slice(-2);
-  // console.log(local);
+ 
 
   var d1=document.getElementById("timings2").value;
-  var value2=d1.slice(0,1);
+  var value2=d1.slice(0,2);
+  // console.log(value2);
   var local2=d1.slice(-2);
   // console.log(local2);
 
   var d2=document.getElementById("timings3").value;
-  var value3=d2.slice(0,1);
+  var value3=d2.slice(0,2);
   var local3=d2.slice(-2);
   // console.log(value3);
   // console.log(local3);  
-  
-  
-
-  if((value1==hh) && (local==x)){
-    var g=document.getElementById("picnic");
-    g.src="wakeup.png"
-    var say=document.getElementById("five1")
-    say.innerText="**lets have some breakfast **"
-    say.appendChild(g);
-  }
-  else if((value2==hh) && (local2==x)){
-    var r=document.getElementById("picnic");
-    r.src="lunch.png";
-    var say1=document.getElementById("five1")
-    say1.innerText="lets have some lunch!!"
-    say1.appendChild(r);
-  }
-  else if ((value3==hh) && (local3==x)){
-    var e=document.getElementById("picnic");
-    e.src="night.png";
-    var say2=document.getElementById("five1")
-    say2.innerText="lets get some sleep!!!"
-    say2.appendChild(e);
-
-  }
-  else{
-      const g3=document.getElementById("picnic");
-      g3.src="noo.jpg";
-      const say3=document.getElementById("five1")
-      say3.innerText="Hii buddy,Hope ur doing well :)"
-      say3.style.fontSize="30px";
-      say3.style.color="hotpink";
-      say3.style.marginTop="10px";
 
 
-  }
+if((value1==hh) && (local==x)){
+  var g=document.getElementById("picnic");
+  g.src="wakeup.png"
+  var say=document.getElementById("five1")
+  say.innerText="**lets have some breakfast **"
+  say.appendChild(g);
+}
+else if((value2==hh) && (local2==x)){
+  var r=document.getElementById("picnic");
+  r.src="lunch.png";
+  var say1=document.getElementById("five1")
+  say1.innerText="lets have some lunch!!"
+  say1.appendChild(r);
+}
+else if ((value3==hh) && (local3==x)){
+  var e=document.getElementById("picnic");
+  e.src="night.png";
+  var say2=document.getElementById("five1")
+  say2.innerText="lets get some sleep!!!"
+  say2.appendChild(e);
+
+}
+else{
+  var g3=document.getElementById("picnic");
+  g3.src="noo.jpg";
+  var say3=document.getElementById("five1")
+  say3.innerText="Hii buddy,Hope ur doing well :)"
+  say3.style.fontSize="30px";
+  say3.style.color="hotpink";
+  say3.style.marginTop="10px";
+  say3.appendChild(g3);
+
+
+}
+ 
+ 
   
 }
-setInterval(timer,1000);
+setInterval(timer);
 
-
-
-const hii=()=>
+function hii()
 {
   var c=document.getElementById("a1");
   c.setAttribute("class","a2");
@@ -95,11 +97,11 @@ const hii=()=>
   var c3=document.getElementById("a7");
   c3.setAttribute("class","a8");
 
-  const change=()=>
+  function change()
   {
     var d=document.getElementById("timings").value;
-    var value1=d.slice(0,1);
-    var ampm=d.slice(-2);
+    var value1=d.slice(0,4);
+    // var ampm=d.slice(-2);
     // console.log(value1);
   
     var d1=document.getElementById("timings2").value;
@@ -135,9 +137,12 @@ const hii=()=>
     {
       document.getElementById("a7").innerText="dinner time:"+d2;
     }
+    
   }
   change()
+  
 }
+
 
 
 
